3ec19a71559035099db8f4c089f95d5d47ca0c8d
Laignier <michellelaignier17@gmail.com>
2023-08-07 21:21:44 -0300

    volume focus fixed

--- js/sounds/cardsFocus.js
+++ js/sounds/cardsFocus.js
@@ -2,11 +2,11 @@ import * as sounds from "./sounds.js"
 import * as elements from "../FocusTimer/elements.js"
 
 export function forestFocusAndHoverActive (sound) {
-    elements.forest.classList.add("card-focus")
-    elements.pathForest.setAttribute("style", "fill: white;")
-    elements.volumeForest.setAttribute("style", "background-color: white;")
-    
-    if(sound != elements.forest && sound != elements.svgForest) { 
+    if(sound == elements.forest || sound == elements.volumeForest || sound == elements.svgForest) {
+        elements.forest.classList.add("card-focus")
+        elements.pathForest.setAttribute("style", "fill: white;")
+        elements.volumeForest.setAttribute("style", "background-color: white;")
+    } else if(sound != elements.forest && sound != elements.svgForest) { 
         elements.forest.classList.remove("card-focus")
         elements.pathForest.removeAttribute("style")
         elements.volumeForest.removeAttribute("style")
@@ -14,11 +14,11 @@ export function forestFocusAndHoverActive (sound) {
 }
 
 export function rainFocusAndHoverActive (sound) {
-    elements.rain.classList.add("card-focus")
-    elements.pathRain.setAttribute("style", "fill: white;")
-    elements.volumeRain.setAttribute("style", "background-color: white;")
-
-    if(sound != elements.rain && sound != elements.svgRain) { 
+    if(sound == elements.rain|| sound == elements.volumeRain || sound == elements.svgRain) {
+        elements.rain.classList.add("card-focus")
+        elements.pathRain.setAttribute("style", "fill: white;")
+        elements.volumeRain.setAttribute("style", "background-color: white;")
+    } else if(sound != elements.rain && sound != elements.svgRain) { 
         elements.rain.classList.remove("card-focus")
         elements.pathRain.removeAttribute("style")
         elements.volumeRain.removeAttribute("style")
@@ -26,11 +26,11 @@ export function rainFocusAndHoverActive (sound) {
 }
 
 export function restaurantFocusAndHoverActive (sound) {
-    elements.restaurant.classList.add("card-focus")
-    elements.pathRestaurant.setAttribute("style", "fill: white;")
-    elements.volumeRestaurant.setAttribute("style", "background-color: white;")
-
-    if(sound != elements.restaurant && sound != elements.svgRestaurant) { 
+    if(sound == elements.restaurant || sound == elements.volumeRestaurant || sound == elements.svgRestaurant) {
+        elements.restaurant.classList.add("card-focus")
+        elements.pathRestaurant.setAttribute("style", "fill: white;")
+        elements.volumeRestaurant.setAttribute("style", "background-color: white;")
+    } else if(sound != elements.restaurant && sound != elements.svgRestaurant) { 
         elements.restaurant.classList.remove("card-focus")
         elements.pathRestaurant.removeAttribute("style")
         elements.volumeRestaurant.removeAttribute("style")
@@ -38,11 +38,11 @@ export function restaurantFocusAndHoverActive (sound) {
 }
 
 export function fireFocusAndHoverActive (sound) {
-    elements.campFire.classList.add("card-focus")
-    elements.pathFire.setAttribute("style", "fill: white;")
-    elements.volumeFire.setAttribute("style", "background-color: white;")
-
-    if(sound != elements.campFire && sound != elements.svgFire) { 
+    if(sound == elements.campFire || sound == elements.volumeFire || sound == elements.svgFire) {
+        elements.campFire.classList.add("card-focus")
+        elements.pathFire.setAttribute("style", "fill: white;")
+        elements.volumeFire.setAttribute("style", "background-color: white;")
+    } else if(sound != elements.campFire && sound != elements.svgFire) { 
         elements.campFire.classList.remove("card-focus")
         elements.pathFire.removeAttribute("style")
         elements.volumeFire.removeAttribute("style")
